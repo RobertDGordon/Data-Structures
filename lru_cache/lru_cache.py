d26057f1cc23aaf086082c0fb714819ee98e02c1
Robert Gordon <Komeuppance@gmail.com>
2020-04-21 23:33:17 -0700

    lru_cache success

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,15 @@
+import sys
+sys.path.append('../doubly_linked_list')
+from doubly_linked_list import DoublyLinkedList
+
+class dll(object):
+    
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -6,9 +18,23 @@ class LRUCache:
     order, as well as a storage dict that provides fast access
     to every node stored in the cache.
     """
-    def __init__(self, limit=10):
-        pass
 
+    def __init__(self, limit=10):
+        self.head = dll(-1, -1)
+        self.tail = self.head
+        self.cache = {}
+        self.capacity = limit
+        self.length = 0
+    
+    def move_to_end(self, node):
+        while node.next:
+            node.prev.next = node.next
+            node.next.prev = node.prev
+            self.tail.next = node
+            node.prev = self.tail
+            node.next = None
+            self.tail = node
+        
     """
     Retrieves the value associated with the given key. Also
     needs to move the key-value pair to the end of the order
@@ -17,7 +43,15 @@ class LRUCache:
     key-value pair doesn't exist in the cache.
     """
     def get(self, key):
-        pass
+        if key not in self.cache:
+            return None
+        node = self.cache[key]
+        value = node.value
+        #move node to end of linked list
+        # self.storage.move_to_end(node)
+        self.move_to_end(node)
+        return value
+            
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -30,4 +64,43 @@ class LRUCache:
     the newly-specified value.
     """
     def set(self, key, value):
-        pass
+        #check if key is in cache
+
+        if key in self.cache:
+            node = self.cache[key]
+            node.value = value
+            #move to end
+            # self.storage.move_to_end(node)
+            self.move_to_end(node)  
+        else:
+            #create new node and insert at end
+            # print(key)
+            # self.storage.add_to_tail(value, key)
+            node = dll(key, value)
+            self.cache[key] = node
+            self.tail.next = node
+            node.prev = self.tail
+            self.tail = node
+            self.length += 1
+            #check length
+            if self.length > self.capacity:
+                #if at capacity remove head
+                # self.storage.remove_from_head()
+                remove = self.head.next
+                self.head.next = self.head.next.next
+                self.head.next.prev = self.head
+                del self.cache[remove.key]
+                self.length -= 1
+
+test = LRUCache(3)
+test.set('item1', 'a')
+test.set('item2', 'b')
+test.set('item3', 'c')
+test.set('item4', 'd')
+test.set('item3', 'h')
+# print(test.storage.get_max())
+print(test.get('item3'))
+print(test.get('item1'))
+print(test.cache['item2'].next)
+# print(test.capacity)
+# print(test.length)
